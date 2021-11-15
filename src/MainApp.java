5533efd7434928a009bb94c60d62538ca75bba6f
Elżbieta Sikora <elzbieta.karolina.94@gmail.com>
2021-11-15 19:57:56 +0100

    typy zmiennych2

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,9 +1,43 @@
 public class MainApp {
 
     public static void main(String[] args) {
-        // Start aplikacji
 
-        System.out.println("Hello Java");
+        //liczby calkowite
+
+        byte b; //typ zmiennej zalezy od wielkosci;
+        short s;
+        int i;
+        long l;
+
+        int number10; //deklaracja
+        number10 = 10; //inicjalizacja zmiennej
+
+        int number = 10;
+        System.out.println(number);
+
+
+        float f; //na mniejsze liczby przecinkowe do 32
+        double d; // liczba zmiennoprzecinkowa o podwojnej precyzji
+
+        double doublenumb = 10.5;
+        System.out.println(doublenumb);
+
+        // sa typy porste i zlozone proste (mala litera) zlozone/referencyjne duzymi czyli jak duzego pudelka chcemy uzywac do przetrzymywania danych
+
+        String someText = "jakis tekst";
+        System.out.println(someText);
+
+        char someChar = 'z';
+        System.out.println(someChar);
+        //w pojedynczych ciapkach pojedynczy znak
+
+        boolean trueValu = true;
+        System.out.println(true);
+
+        boolean falseValue = false;
+        System.out.println(false);
+        //boolean
+
     }
 }
 
