62cdefcd423a252ea685bc151f7e861746d66c53
Elżbieta Sikora <elzbieta.karolina.94@gmail.com>
2021-11-18 18:27:23 +0100

    Operatory logiczne i funkcje sterujace

--- src/OperatoryLogiczne.java
+++ src/OperatoryLogiczne.java
@@ -41,5 +41,66 @@ public class OperatoryLogiczne {
         System.out.println(numOne < numTwo && numOne == 1);
         System.out.println(numOne > numTwo && numOne == 1);
         System.out.println(numOne > numTwo && numOne != 1);
+
+
+        //instrukcje sterujacace pomaga okreslic jaka czesc kod powinna byc wykonana przy jakim warunku
+
+        if (true) {
+            System.out.println("Jestem kodem z ifa - true");
+        }
+
+        if (numOne < numTwo) {
+            System.out.println("number one is less than number two");
+        } else { //w przeciwnym razie
+            System.out.println("number one is gerater than number two");
+        }
+
+        //Instrukcja sterujaca "switch"
+
+        String homePage = "Home";
+        String loaginPage = "Login";
+        String contactPage = "Contact";
+
+        String page = homePage;
+
+        switch (page) {
+            case "Home":
+                System.out.println("przelaczam sie do strony home");
+                break;
+            case "Login":
+                System.out.println("przelaczam sie do strony login");
+                break;
+            case "Contact":
+                System.out.println("przelaczam sie do strony contact");
+                break;
+            default:
+                System.out.println("Nie znam takiej strony");
+
+        }
+
+        //Nazwy klas PascalCase (duza i duza)
+        //User, SuperUser, AdminUser
+
+        //zmienne i metody camelCase (mala i duza)
+        // superUser
+
+        String superUser = "jestem super user";
+        int number = 10;
+        boolean isTrue = true;
+
+        //formatowanie kodu alt+com +l
+
+        //operator modulo to operator reszty z dzielenia (modulo to %) ile wartosci miesci sie w jakiejs wartosci plus wyswietlamy reszta ktora zostaje
+
+        System.out.println(10 % 5);
+        System.out.println(10 % 3);
+        System.out.println(10 % 4);
+
+        if(9 % 2 == 0) {
+            System.out.println("nie jestem liczba parzysta");
+        } else {
+            System.out.println("nie jesyem liczba parzysta");
+        }
+
     }
 }
